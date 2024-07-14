05fd2788f3c9a79f36692b94c2168c24b5fde439
Divyadharshini-A <116701382+Divyadharshini-A@users.noreply.github.com>
2024-07-14 23:08:20 +0530

    Update Myclass.java

--- Databaseconnectivity/src/database/Myclass.java
+++ Databaseconnectivity/src/database/Myclass.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Myclass {
 	 private static final String URL = "jdbc:mysql://localhost:3301/Hotel";
 	    private static final String USER = "root";
-	    private static final String PASSWORD = "Divya@273";
+	    private static final String PASSWORD = "Nishanthini@1010";
 
 	    public static Connection getConnection() throws SQLException {
 	        return DriverManager.getConnection(URL, USER, PASSWORD);
